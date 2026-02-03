dedeb33f2cc9aa4b55c43e831624b64dabd29597
Jeremiah Oriowo <jeremiahoriowo@gmail.com>
2026-02-03 19:43:17 +0100

    configure next.config to exclude openai from server components bundling

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: ['openai'],
+  },
+  // Disable static page generation for API routes
+  generateBuildId: async () => {
+    return 'build-' + Date.now()
+  },
 }
 
 module.exports = nextConfig
